9afbaababdff9da13b6722c5281c8413aa1ca024
tiberx <tiberx@smg.sh.cn>
2014-06-20 13:36:22 +0800

    Create xukangwenViewController.h

---
+++ xukangwenViewController.h
@@ -0,0 +1,22 @@
+//使用方法
+
+//
+//  xukangwenViewController.h
+//  VideoByVitiomDemo
+//
+//  Created by zhengyu xu on 14-6-19.
+//  Copyright (c) 2014年 smg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "KKWVideoView.h"
+
+@interface xukangwenViewController : UIViewController<KKWVideoViewDelegate >{
+
+
+    bool _isFullScreen;
+
+    
+}
+
+@end
